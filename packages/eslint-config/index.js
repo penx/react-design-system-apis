ca846efe0a9079d0c92e8fcee525881b39bcfa96
Alasdair McLeay <alasdair.mcleay@me.com>
2026-06-20 15:03:40 +0100

    tooling: React ESLint (eslint-plugin-react-hooks v7 + react-compiler)

--- packages/eslint-config/index.js
+++ packages/eslint-config/index.js
@@ -8,12 +8,22 @@ module.exports = {
     "plugin:@typescript-eslint/recommended",
     "prettier",
   ],
-  plugins: ["@typescript-eslint"],
+  // react-hooks v7 + react-compiler ship flat-config presets; enable the rules
+  // explicitly so this legacy (.eslintrc) config stays version-robust.
+  plugins: ["@typescript-eslint", "react-hooks", "react-compiler"],
   parserOptions: {
     sourceType: "module",
     ecmaVersion: 2020,
   },
   rules: {
     "@typescript-eslint/no-non-null-assertion": "off",
+    "react-hooks/rules-of-hooks": "error",
+    "react-hooks/exhaustive-deps": "warn",
+    // Rules of React from react-hooks v7: no ref read/write during render, and
+    // no setState during render.
+    "react-hooks/refs": "error",
+    "react-hooks/set-state-in-render": "error",
+    // React Compiler's holistic bail-out check.
+    "react-compiler/react-compiler": "error",
   },
 };
