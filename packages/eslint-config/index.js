ab88a6b6ce5c2bafbffdb0124b8212fb0330c320
Alasdair McLeay <alasdair.mcleay@me.com>
2026-07-07 21:56:12 +0100

    chore: drop explanatory comments from eslint config

--- packages/eslint-config/index.js
+++ packages/eslint-config/index.js
@@ -8,8 +8,6 @@ module.exports = {
     "plugin:@typescript-eslint/recommended",
     "prettier",
   ],
-  // react-hooks v7 + react-compiler ship flat-config presets; enable the rules
-  // explicitly so this legacy (.eslintrc) config stays version-robust.
   plugins: ["@typescript-eslint", "react-hooks", "react-compiler"],
   parserOptions: {
     sourceType: "module",
@@ -19,11 +17,8 @@ module.exports = {
     "@typescript-eslint/no-non-null-assertion": "off",
     "react-hooks/rules-of-hooks": "error",
     "react-hooks/exhaustive-deps": "error",
-    // Rules of React from react-hooks v7: no ref read/write during render, and
-    // no setState during render.
     "react-hooks/refs": "error",
     "react-hooks/set-state-in-render": "error",
-    // React Compiler's holistic bail-out check.
     "react-compiler/react-compiler": "error",
   },
 };
