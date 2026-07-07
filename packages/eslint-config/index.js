ffd1567d9808c787f362eb1d97bfbaa5d28265a8
Alasdair McLeay <penx@users.noreply.github.com>
2026-07-07 21:57:10 +0100

    tooling: React ESLint (react-hooks v7 + react-compiler) (#5)
    * tooling: React ESLint (eslint-plugin-react-hooks v7 + react-compiler)
    
    * ci: run lint on PRs; promote exhaustive-deps to error
    
    - add .github/workflows/ci.yml running pnpm lint on pull_request and
      pushes to main (pnpm 8.15.6 via packageManager, Node 20)
    - react-hooks/exhaustive-deps warn -> error; no existing violations
    
    * chore: drop explanatory comments from eslint config

--- packages/eslint-config/index.js
+++ packages/eslint-config/index.js
@@ -8,12 +8,17 @@ module.exports = {
     "plugin:@typescript-eslint/recommended",
     "prettier",
   ],
-  plugins: ["@typescript-eslint"],
+  plugins: ["@typescript-eslint", "react-hooks", "react-compiler"],
   parserOptions: {
     sourceType: "module",
     ecmaVersion: 2020,
   },
   rules: {
     "@typescript-eslint/no-non-null-assertion": "off",
+    "react-hooks/rules-of-hooks": "error",
+    "react-hooks/exhaustive-deps": "error",
+    "react-hooks/refs": "error",
+    "react-hooks/set-state-in-render": "error",
+    "react-compiler/react-compiler": "error",
   },
 };
